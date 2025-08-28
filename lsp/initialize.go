d14d655163f9475ae7df5e077cc4c765921b83bb
Harshwardhan <135024692+harsh-m-patil@users.noreply.github.com>
2025-08-29 00:59:09 +0530

    Respond to initialize method

---
+++ lsp/initialize.go
@@ -0,0 +1,49 @@
+package lsp
+
+type InitializeRequest struct {
+	Request
+	Params InitializeRequestParams `json:"params"`
+}
+
+type InitializeRequestParams struct {
+	ClientInfo *ClientInfo `json:"clientInfo"`
+	// more fields exist, but we don't need them
+}
+
+type ClientInfo struct {
+	Name    string `json:"name"`
+	Version string `json:"version"`
+}
+
+type InitializeResponse struct {
+	Response
+	Result InitializeResult `json:"result"`
+}
+
+type InitializeResult struct {
+	Capabilities ServerCapabilities `json:"capabilities"`
+	ServerInfo   ServerInfo         `json:"serverInfo"`
+}
+
+type ServerCapabilities struct{}
+
+type ServerInfo struct {
+	Name    string `json:"name"`
+	Version string `json:"version"`
+}
+
+func NewInitializeResponse(id int) InitializeResponse {
+	return InitializeResponse{
+		Response: Response{
+			RPC: "2.0",
+			ID:  &id,
+		},
+		Result: InitializeResult{
+			Capabilities: ServerCapabilities{},
+			ServerInfo: ServerInfo{
+				Name:    "lspfromscratch",
+				Version: "0.1.0",
+			},
+		},
+	}
+}
--- main.go
+++ main.go
@@ -2,7 +2,9 @@ package main
 
 import (
 	"bufio"
+	"encoding/json"
 	"log"
+	"lspfromscratch/lsp"
 	"lspfromscratch/rpc"
 	"os"
 )
@@ -16,13 +18,40 @@ func main() {
 	scanner.Split(rpc.Split)
 
 	for scanner.Scan() {
-		msg := scanner.Text()
-		handleMessage(logger, msg)
+		msg := scanner.Bytes()
+		method, content, err := rpc.DecodeMessage(msg)
+		if err != nil {
+			logger.Println("Error decoding message:", err)
+			continue
+		}
+
+		handleMessage(logger, method, content)
 	}
 }
 
-func handleMessage(logger *log.Logger, msg any) {
-	logger.Println(msg)
+func handleMessage(logger *log.Logger, method string, contents []byte) {
+	logger.Println("Received message with method:", method)
+
+	switch method {
+	case "initialize":
+		var request lsp.InitializeRequest
+		if err := json.Unmarshal(contents, &request); err != nil {
+			logger.Println("Error unmarshaling initialize request:", err)
+			return
+		}
+
+		logger.Printf("Connected to: %s %s\n",
+			request.Params.ClientInfo.Name,
+			request.Params.ClientInfo.Version)
+
+		// reply
+		msg := lsp.NewInitializeResponse(request.ID)
+		reply := rpc.EncodeMessage(msg)
+		writer := os.Stdout
+		writer.Write([]byte(reply))
+
+		logger.Println("Sent initialize response")
+	}
 }
 
 func getLogger(filename string) *log.Logger {
